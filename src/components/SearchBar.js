f76879f13d7fbc91688ec5fa7daeb93f11571d82
Clara-Ro <lara.rhee.gree@gmail.com>
2022-02-16 20:05:39 -0300

    Search Bar componentizada

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import { SearchIcon } from "../assents/Icons"
+
+export default function SearchBar({userName,handleInput,searchUser,loading}) {
+    return (
+        <div className='search'>
+            <SearchIcon />
+            <input className='search-input' placeholder='Search GitHub username...'
+                value={userName} onChange={handleInput} />
+            <button className='search-button' onClick={searchUser} disabled={loading}>Search</button>
+        </div>
+    )
+}
