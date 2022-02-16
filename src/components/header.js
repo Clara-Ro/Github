e534fbc981a19a8a1c4b62bd43a617c024a44bb7
Clara-Ro <lara.rhee.gree@gmail.com>
2022-02-16 19:37:19 -0300

    header componentizado

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import{Sun, Moon} from '../assents/Icons/'
+
+export default function Header({ changeTheme, isDark }){
+    return (
+        <header>
+            <h1 className='title'>devfinder</h1>
+            <div className='theme' onClick={changeTheme}>
+                <span>{isDark ? 'LIGHT' : 'DARK'}</span>
+                {isDark ? <Sun /> : <Moon />}
+            </div>
+        </header>
+    )
+}
